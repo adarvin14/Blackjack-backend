236120d38c7997b21ecc18ebddcaa238ea56ea76
adarvin14 <amdarvin14@gmail.com>
2021-04-10 16:05:33 -0400

    create book model/seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create(title: "title 1", author: "author 1", review: "review 1")
+Book.create(title: "title 2", author: "author 2", review: "review 2")
+Book.create(title: "title 3", author: "author 3", review: "review 3")
+Book.create(title: "title 4", author: "author 4", review: "review 4")
